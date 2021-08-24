f0f6a25716f57b1fe8f218f8cb8f4664c8efff85
Nick Stockton <phoenix76728@gmail.com>
2021-08-24 10:05:04 -0400

    initial commit

---
+++ SugarSyntaxExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SugarSyntaxExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;    //changed int to var
+
+            // changed the if statment to a Ternary Operator
+
+
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} greater than or equal to nine"; 
+
+            // added interpolation to response 
+
+            Console.WriteLine(response);
+        }
+    }
+}
